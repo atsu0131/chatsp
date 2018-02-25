879c5f0083ddbb95eb8b7435025f68ea58e602b2
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-02-25 18:21:22 +0900

    グループメンバー追加、削除機能追加

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,28 +9,30 @@ $(function(){
       return html;
   };
 
-$(document).delegate('a','click',function(){
+  function removeuser (userid,username){
+    var html =
+      `
+      <div class = search-result>
+        <input name='group[user_ids][]' type='hidden' value=${userid}>
+        <div class= name-display >${username}</div>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+        </div>`
+      return html
+}
+
+$(document).delegate('.user-search-add','click',function(){
       var user_id = $(this).data("user-id");
       var user_name = $(this).data("user-name");
+      console.log($(this).parent())
       $(this).parent().remove();
-      $(".chat-group-user-22").append(`<input name="group[user_ids][] "type="hidden" value=${user_id}></input><div class= name-display >${user_name}</div>`)
+      $(".chat-group-user-22").append(removeuser(user_id,user_name));
 });
 
 
-//   function removeuser(){
-//     var html =
-//       `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-//         <input name='group[user_ids][]' type='hidden' value=${user.id}>
-//         <p class='chat-group-user__name'>${user.name}</p>
-//         <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-//       </div>`
-//       return html
-// }
+$(document).delegate('.user-search-remove','click',function(){
+  $(this).parent().remove();
+});
 
-//   $(".chat-group-user").on("click", ".chat-group-user__btn--remove", function() {
-//   var user_id = $(this).data("user-id");
-//   $(this).parent().remove();
-//     });
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
@@ -42,14 +44,13 @@ $(document).delegate('a','click',function(){
       dataType: 'json'
     })
     .done(function(users) {
-      $('.chat-group-form-box').empty();
      if (users.length !== 0) {
        users.forEach(function(user){
          var html = appenduser(user);
          $('.chat-group-form-box').append(html);
        });
       }else {
-      $('.chat-group-form-box').empty();
+      $('.chat-group-form-box').remove();
     }
     })
     .fail(function() {
